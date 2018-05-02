f4e02279dbac926710183bfcce209cd98609dacf
Derian Pinto <pintoderian@gmail.com>
2018-05-01 21:38:28 -0500

    Personalizar modelo

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post 
 #importanción relativa
+
 # Register your models here.
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('__str__','actualizado', 'timestamp')
+    #list_display_links = ["actualizado"] para que el link este en la fecha y no en el titulo
+    list_filter = ['timestamp']
+    search_fields = ['titulo', 'contenido']
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Post(models.Model):
     titulo = models.CharField(max_length=150)
     contenido = models.TextField()
-    timestamp = models.TimeField(auto_now_add=True, auto_now=False)
-    actualizado = models.TimeField(auto_now_add=False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
         return '%s' % self.titulo
