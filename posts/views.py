9eaea839738392376831eff45c0807d2b8f4f410
Derian Pinto <pintoderian@gmail.com>
2018-05-04 10:51:10 -0500

    Consulta en la base de datos

--- posts/views.py
+++ posts/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse #importado
 
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 def post_home(request):
     #return HttpResponse("<h1>Post home!</h1>")
+    query = Post.objects.all()
     context = {
-        "titulo": "Blog con Django"
+        "titulo": "Blog con Django",
+        "posts": query
     }
     return render(request, "index.html", context)
